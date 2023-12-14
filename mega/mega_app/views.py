36c0317380de2abab8c2087a23c1acc7d135d5ba
InFluX-M <matinazami2003@gmail.com>
2023-12-14 17:54:04 +0330

    complete

--- mega/mega_app/views.py
+++ mega/mega_app/views.py
@@ -3,15 +3,24 @@ from django.http import JsonResponse
 from django.shortcuts import render
 import serial
 import time
+from django.views.decorators.csrf import csrf_exempt
 
-ser = serial.Serial('COM7', 9600, timeout=1)
+import logging
+
+
+logger = logging.Logger(__name__)
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 blink_state = 0
 T = 1000
 
+@csrf_exempt 
 def index(request):
     return render(request, 'index.html')
 
+@csrf_exempt 
 def send_command(request):
+    global logger, ser, blink_state, T
+    
     if request.method == 'POST':
         command = request.POST.get('command', '')
         ser.write((command + '\r' + '\n').encode())
@@ -31,10 +40,10 @@ def send_command(request):
             else:
                 T = int(command[7:])
     
-        time.sleep(1)
+        time.sleep(1.5)
 
         res = ser.readline().decode().strip()
-        response_from_arduino = f"{res} response for command: {command}"
+        response_from_arduino = f"{res}"
         return JsonResponse({'response': response_from_arduino})
 
     return JsonResponse({'error': 'Invalid request method'})
